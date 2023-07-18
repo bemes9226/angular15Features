7c0d06a7fe0d58de9ff06a32cb73d3daf891426a
bemes9226 <bemes9226@gmail.com>
2023-07-19 02:56:02 +0530

    code moved to child component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BoldDirective } from '../bold.directive';
+import { ColorDirective } from '../color.directive';
+import { FooDirective } from '../foo.directive';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatToolbarModule, FooDirective, ColorDirective, BoldDirective],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  title = 'angular15Features';
+  color = '';
+  weight = '';
+  showAlert() {
+    alert("hello");
+  }
 }
