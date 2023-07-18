196b2985258dbff27aa8d33b8a4260185269773e
bemes9226 <bemes9226@gmail.com>
2023-07-19 00:58:10 +0530

    angular 14 page title

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,11 @@ import { HomeComponent } from './app/home/home.component';
 import { ProductComponent } from './app/product/product.component';
 
 const routes=[{path:'home', 
-              component:HomeComponent},
+              component:HomeComponent,
+              title:'Home page'},
               {path:'product',
-               loadComponent:()=>import('./app/product/product.component').then(res=>res.ProductComponent)}]
+               loadComponent:()=>import('./app/product/product.component').then(res=>res.ProductComponent),
+              title:'product page'}]
 bootstrapApplication(AppComponent,{
   providers:[importProvidersFrom(RouterModule.forRoot(routes))]
 }).catch(e=>console.log('e'));
